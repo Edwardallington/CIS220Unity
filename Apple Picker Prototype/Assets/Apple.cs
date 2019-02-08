64b7f1e57e76bab9ac32d67c4d31fc834f512e30
edwardallington <ea0911399@otc.edu>
2019-02-08 10:17:18 -0600

    Updated Apple Picker Game

--- Apple Picker Prototype/Assets/Apple.cs
+++ Apple Picker Prototype/Assets/Apple.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Apple : MonoBehaviour {
-
+    // static variable - only one of these for all apples
+    public static float bottomY = -15f;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +12,12 @@ public class Apple : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // if we are past the bottom, destroy the apple
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
+        }
 	}
 }
